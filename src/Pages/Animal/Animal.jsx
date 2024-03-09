dd0434ae79572ae548a96589fbf89b0cf6e5debf
nidaonder <nidaaonder@gmail.com>
2024-03-09 10:20:11 +0300

    added customerlist.

--- src/Pages/Animal/Animal.jsx
+++ src/Pages/Animal/Animal.jsx
@@ -1,8 +1,5 @@
-import { useState } from "react"
-
 
 export default function Animal() {
-  const [customer, setCustomer] = useState([]);
   return (
     <h1>Animal</h1>
   )
--- src/Pages/Customer/Customer.jsx
+++ src/Pages/Customer/Customer.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import "./Customer.css";
 
 function Customer() {
+  const [customer, setCustomer] = useState([]);
+  const [reload, setReload] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/customers")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setCustomer(data);
+      });
+      setReload(false);
+  }, [reload]);
+
+  const handleDelete = (event) => {
+    const id = event.target.id;
+    fetch(`http://localhost:8080/api/v1/customers/${id}`, {
+      method: "DELETE",
+    })
+    .then(() => {
+      setReload(true);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+  };
+
   return (
-    <h1>Customer</h1>
-  )
+    <>
+      <h1>Customer</h1>
+      <div className="customer-list">
+        {customer.map((customer) => (
+          <div 
+          id={customer.id}
+          onClick={(e) => handleDelete(e)} 
+          key={customer.id}>{customer.name}
+          </div>
+        ))}
+        </div>
+    </>
+  );
 }
 
-export default Customer
+export default Customer;
